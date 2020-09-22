550dd22b8a5b84547f8174f51bc59311f8afe060
penguin418 <penguin418@naver.com>
2020-09-22 11:58:32 +0900

    feat: Added chat room
    Used subscribtion model and socketio room to support multiple chat rooms

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 #!/bin/env python
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from time import time
 
 
 socketio = SocketIO()
@@ -17,9 +18,24 @@ def create_app(debug=False):
     # socketio 코드 시작 // start socketio code
     socketio.init_app(app)
 
+    @socketio.on('subscribe')
+    def subscribe(data):
+        print('subscribe ', data['room'], request.sid)
+        join_room(data['room'])
+
+    @socketio.on('unsubscribe')
+    def unsubscribe(data):
+        print('unsubscribe ', data['room'], request.sid)
+        leave_room(data['room'])
+
     @socketio.on('send msg')
     def send_msg(data):
         print(data)
-        emit('broadcast msg', data, broadcast=True)
-        
+        data['timestamp'] = time()
+        emit(
+            'broadcast msg', 
+            data,
+            room=data['room']
+            )
+            
     return app

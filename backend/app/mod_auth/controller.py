8f669b0e04b0a10252084964fd2cbaaa1ce1ed1c
penguin418 <penguin418@naver.com>
2020-09-25 12:16:38 +0900

    test: Tested register

--- backend/app/mod_auth/controller.py
+++ backend/app/mod_auth/controller.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, jsonify
 from . import mod_auth
 from .. import app
+import flask_bcrypt
 
 
 @mod_auth.route('/db', methods=['POST'])
@@ -15,6 +16,18 @@ def dbtest():
 
     return jsonify({'success': 'success'}), 200
 
+@mod_auth.route('/register', methods=['POST'])
+def register():
+    if not request.is_json:
+        return jsonify({"error": "요청은 json이어야 합니다 // request should be JSON"}), 400
+    
+    data = request.get_json()
+    if data['username'] and data['email'] and data['password']:
+        data['password'] = flask_bcrypt.generate_password_hash(data['password'])
+        app.db['users'].insert_one(data)
+        res = jsonify({'success': '등록되었습니다 // registered successfully!'})
+        return res, 200
+    return jsonify({'error': 'wrong parameters'}), 400
 
 @mod_auth.route('/test')
 def test():
